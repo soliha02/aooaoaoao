0264167c3596110f69286a481034180b9e68a42e
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-06-09 12:16:16 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+let start =('Boshlanishda arryning uzunligi 4 ')
+let omg =prompt('Sizning arryingizda malumotlarni olib tashlamoqchimiz')
+let finish=prompt('Boshlanishda arryning uzunligi 3 ')
+alert(start,omg,finish)
+
+
+let mevalar = ["olma" , "banan" , "gilos" , "shaftoli"] 
+mevalar.pop()
+console.log(mevalar)
+
+
+
